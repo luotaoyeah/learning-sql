41d3ae2e19c9d45219f97bf89533baf81f4e1768
luotao <luotaoyeah@gmail.com>
2023-09-28 17:48:24 +0800

    feat(projects.learning-sql): 4.3.3. membership conditions

---
+++ projects/learning-sql/src/04/03/03.sql
@@ -0,0 +1,10 @@
+-- 4.3.3. membership conditions
+
+
+USE sakila;
+
+-- IN 后面可以使用子查询, 子查询的结果集只能包含一个列,
+
+SELECT title, rating
+  FROM film
+ WHERE rating IN (SELECT rating FROM film WHERE title LIKE '%PET%');
