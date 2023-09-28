f4a3e31b000ce18034a0d2fc33ecddbf672fb287
luotao <luotaoyeah@gmail.com>
2023-09-28 20:25:13 +0800

    feat(projects.learning-sql): 5.1.1. cartesian product

---
+++ projects/learning-sql/src/05/01/01.sql
@@ -0,0 +1,10 @@
+-- 5.1.1. cartesian product
+
+
+USE sakila;
+
+-- 如果 JOIN 语句不加 ON 条件, 则结果是两个表的所有行组成的笛卡尔积, 这种叫做 CROSS JOIN,
+
+SELECT first_name, last_name, address
+  FROM customer
+           INNER JOIN address;
