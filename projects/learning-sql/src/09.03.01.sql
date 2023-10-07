34877edbe719effcc1101bba43acf6847ec029af
luotao <luotaoyeah@gmail.com>
2023-10-07 23:24:14 +0800

    feat(projects.learning-sql): 9.3.1. multiple-row, single-column subqueries

--- projects/learning-sql/src/09.03.01.sql
+++ projects/learning-sql/src/09.03.01.sql
@@ -16,14 +16,26 @@ SELECT *
 SELECT NULL = NULL;
 
 
--- `x > 1 AND x > 2 AND x > 3` 这种形式可以写成 `x > ALL (1, 2, 3)`, 其中 (1, 2, 3) 需要由子查询产生,
+-- `x > 1 AND x > 2` 这种形式可以写成 `x > ALL (1, 2)`, 其中 (1, 2) 需要由子查询产生,
 
 SELECT x
   FROM ((SELECT 1 AS x) UNION (SELECT 2 AS x) UNION (SELECT 3 AS x) UNION (SELECT 4 AS x)) AS a
  WHERE x > 1
-   AND x > 2
-   AND x > 3;
+   AND x > 2;
 
 SELECT x
   FROM ((SELECT 1 AS x) UNION (SELECT 2 AS x) UNION (SELECT 3 AS x) UNION (SELECT 4 AS x)) AS a
- WHERE x > ALL (SELECT y FROM ((SELECT 1 AS y) UNION (SELECT 2 AS y) UNION (SELECT 3 AS y)) AS b);
+ WHERE x > ALL (SELECT y FROM ((SELECT 1 AS y) UNION (SELECT 2 AS y)) AS b);
+
+
+-- `x > 1 OR x > 2` 这种形式可以写成 `x > ANY (1, 2)`, 其中 (1, 2) 需要由子查询产生,
+-- `x IN (1, 2)` 可以写成 `x = ANY (1, 2)`,
+
+SELECT x
+  FROM ((SELECT 1 AS x) UNION (SELECT 2 AS x) UNION (SELECT 3 AS x) UNION (SELECT 4 AS x)) AS a
+ WHERE x > 1
+    OR x > 2;
+
+SELECT x
+  FROM ((SELECT 1 AS x) UNION (SELECT 2 AS x) UNION (SELECT 3 AS x) UNION (SELECT 4 AS x)) AS a
+ WHERE x > ANY (SELECT y FROM ((SELECT 1 AS y) UNION (SELECT 2 AS y)) AS b);
