c4c89532a14a8d27722dd910f0c036faabfe1548
luotao <luotaoyeah@gmail.com>
2023-10-24 21:38:49 +0800

    feat(projects.t-sql-fundamentals): 2.3. CASE expressions: ISNULL(), COALESCE(), IIF()

--- projects/t-sql-fundamentals/src/02.03.sql
+++ projects/t-sql-fundamentals/src/02.03.sql
@@ -14,3 +14,16 @@ SELECT supplierid,
   FROM production.products
  GROUP BY supplierid;
 
+-- ISNULL(x, y), 接受两个参数, 如果 x 不为 NULL 则结果为 x, 如果 x 为 NULL 则结果为 y,
+
+SELECT ISNULL(NULL, 'FOO');
+
+-- COALESCE(x, y, z, ...), 可以接收多个参数, 结果为第一个不为 NULL 的值,
+
+SELECT COALESCE(NULL, 'FOO', 'BAR', NULL);
+
+-- IIF(), 三元表达式,
+
+SELECT IIF(1 > 0, 'Y', 'N');
+
+
