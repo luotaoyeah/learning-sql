1cc0ff7cce4d969eb1c53389bf34d73adb8b7c8d
luotao <luotaoyeah@gmail.com>
2024-01-01 23:34:11 +0800

    feat(projects.learning-sql): 3.6. the group by and having clauses

--- projects/learning-sql/src/03.06.00.sql
+++ projects/learning-sql/src/03.06.00.sql
@@ -4,26 +4,18 @@
 USE sakila;
 
 
--- HAVING 跟 GROUP BY 配合使用, 作用是对分组后的每一个分组中的行进行过滤, 跟 WHERE 的区别是 WHERE 是对分组前所有的行进行过滤,
--- HAVING 后面可以是聚合函数(对每个分组进行聚合), 比如 `HAVING COUNT(*)`, WHERE 后面不允许是聚合函数,
-
-SELECT first_name, last_name, COUNT(*)
+-- region HAVING
+-- ----------------------------------------------------------------------------------------------------
+-- HAVING 和 WHERE 的区别:
+--   1. WHERE 是对分组前的行进行过滤, 判断是否排除某行, 不能使用聚合函数
+--   2. HAVING 是对分组后的分组进行过滤, 判断是否排除某个分组, 可以使用聚合函数
+SELECT first_name,
+       last_name,
+       COUNT(*)
   FROM customer
            INNER JOIN rental
            ON customer.customer_id = rental.customer_id
  GROUP BY customer.first_name,
           customer.last_name
 HAVING COUNT(*) > 40;
-
-
--- HAVING 可以单独使用(没有 GROUP BY), 此时会有一个隐式的分组(即将所有行放到一个分组中),
--- HAVING 后面也可以是普通的过滤条件, 此时效果就跟使用 WHERE 一样,
--- 如下两个查询效果一样,
-
-SELECT first_name, last_name
-  FROM customer
- WHERE first_name = 'KELLY';
-
-SELECT first_name, last_name
-  FROM customer
-HAVING first_name = 'KELLY';
+-- endregion
