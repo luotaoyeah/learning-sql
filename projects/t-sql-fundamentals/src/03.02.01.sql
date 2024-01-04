cefc07ed5d4b06ddfcab5dc280a653881e471adc
luotao <luotaoyeah@gmail.com>
2024-01-05 01:08:06 +0800

    feat(projects.t-sql-fundamentals): 3.2. inner joins

--- projects/t-sql-fundamentals/src/03.02.01.sql
+++ projects/t-sql-fundamentals/src/03.02.01.sql
@@ -1,9 +1,10 @@
--- 3.1. cross joins
+-- 3.2. inner joins
 
--- region CROSS JOIN
+-- region INNER JOIN
 -- ----------------------------------------------------------------------------------------------------
--- CROSS JOIN 的结果就是两个表的笛卡尔乘积,
--- CROSS JOIN 不能使用 ON 子句,
+-- INNER JOIN 的处理分两步:
+--   1. 跟 CROSS JOIN 一样得到两个表的笛卡尔乘积
+--   2. 根据 ON 子句过滤不满足条件的行
 SELECT t_01.id AS [t_01.id],
        t_01.x,
        t_02.id AS [t_02.id],
@@ -15,11 +16,17 @@ SELECT t_01.id AS [t_01.id],
             UNION ALL
            SELECT 3 AS id, 'C' AS x
        ) AS t_01
-           CROSS JOIN (
+           INNER JOIN (
                           SELECT 2 AS id, N'甲' AS y
                            UNION ALL
                           SELECT 3 AS id, N'乙' AS y
                            UNION ALL
                           SELECT 4 AS id, N'丙' AS y
-                      ) AS t_02;
+                      ) AS t_02
+           ON t_01.id = t_02.id;
+-- endregion
+
+-- region JOIN
+-- ----------------------------------------------------------------------------------------------------
+-- 关键字 INNER JOIN 可以简写为 JOIN,
 -- endregion
