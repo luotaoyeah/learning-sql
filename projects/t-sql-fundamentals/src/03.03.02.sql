bfc41f450537a51bce545ed9e3b37a6d491296b2
luotao <luotaoyeah@gmail.com>
2024-01-05 01:20:33 +0800

    feat(projects.t-sql-fundamentals): 3.3.2. non-equi joins

--- projects/t-sql-fundamentals/src/03.03.02.sql
+++ projects/t-sql-fundamentals/src/03.03.02.sql
@@ -1,12 +1,7 @@
--- 3.2. inner joins
+-- 3.3.2. non-equi joins
 
--- region INNER JOIN
+-- region ON 后面的表达式除了使用 = 判断是否相等, 还可以是任意的条件, 比如 < 等等,
 -- ----------------------------------------------------------------------------------------------------
--- INNER JOIN 的处理分两步:
---   1. 跟 CROSS JOIN 一样得到两个表的笛卡尔乘积
---   2. 根据 ON 子句过滤不满足条件的行
-
--- INNER JOIN 必须跟上 ON 一起使用,
 SELECT t_01.id AS [t_01.id],
        t_01.x,
        t_02.id AS [t_02.id],
@@ -25,10 +20,5 @@ SELECT t_01.id AS [t_01.id],
                            UNION ALL
                           SELECT 4 AS id, N'丙' AS y
                       ) AS t_02
-           ON t_01.id = t_02.id;
--- endregion
-
--- region JOIN
--- ----------------------------------------------------------------------------------------------------
--- 关键字 INNER JOIN 可以简写为 JOIN,
+           ON t_01.id < t_02.id;
 -- endregion
