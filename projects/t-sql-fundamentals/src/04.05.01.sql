c688e2cef7f86621d02fc60d442cc628d330e49f
luotao <luotaoyeah@gmail.com>
2024-01-06 21:32:33 +0800

    feat(projects.t-sql-fundamentals): 4.5.1. NULL trouble

---
+++ projects/t-sql-fundamentals/src/04.05.01.sql
@@ -0,0 +1,32 @@
+-- 4.5.1. NULL trouble
+
+-- region NOT IN (NULL, ...)
+-- ----------------------------------------------------------------------------------------------------
+-- 'TRUE'
+-- 分析步骤如下:
+--     1 IN (1, NULL)
+--     1 = 1 OR 1 = NULL
+--     TRUE OR UNKNOWN
+--     TRUE
+SELECT IIF(1 IN (1, NULL), 'TRUE', 'FALSE');
+
+-- 'FALSE'
+-- 分析步骤如下:
+--     1 IN (2, NULL)
+--     1 = 2 OR 1 = NULL
+--     FALSE OR UNKNOWN
+--     UNKNOWN
+SELECT IIF(1 IN (2, NULL), 'TRUE', 'FALSE');
+
+-- NOT IN 后面如果包含 NULL, 则 NOT IN 表达式的结果始终为 UNKNOWN,
+
+-- 'FALSE'
+-- 分析步骤如下:
+--     1 NOT IN (2, NULL)
+--     NOT (1 IN (2, NULL))
+--     NOT (1 = 2 OR 1 = NULL)
+--     NOT (FALSE OR UNKNOWN)
+--     NOT (UNKNOWN)
+--     UNKNOWN
+SELECT IIF(1 NOT IN (2, NULL), 'TRUE', 'FALSE');
+-- endregion
