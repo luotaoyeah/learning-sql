f574402d6a5b9ba38ef9a80c86f6e6c5f407b4ee
luotao <luotaoyeah@gmail.com>
2024-06-02 04:34:56 +0800

    feat(projects.t-sql-fundamentals): 12.3.2 the WHILE flow element

---
+++ projects/t-sql-fundamentals/src/12.03.02.sql
@@ -0,0 +1,45 @@
+-- 12.3.2 the WHILE flow element
+
+
+DECLARE @i INT = 1;
+
+WHILE @i <= 10 BEGIN
+    PRINT @i;
+    SET @i = @i + 1;
+END;
+
+GO
+
+-- 使用 BREAK 结束所有循环
+
+DECLARE @i INT = 1;
+
+WHILE @i <= 10 BEGIN
+    IF @i = 6
+        BEGIN
+            BREAK ;
+        END
+
+    PRINT @i;
+
+    SET @i = @i + 1;
+END
+
+GO
+
+-- 使用 CONTINUE 结束本次循环
+
+DECLARE @i INT = 0;
+
+WHILE @i <= 10 BEGIN
+    SET @i = @i + 1;
+
+    IF @i = 6
+        BEGIN
+            CONTINUE ;
+        END
+
+    PRINT @i;
+END
+
+GO
